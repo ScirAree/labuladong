b5f036341a30fd20676740e4d88c4bd80b4c1af9
Aree <93140966+ScirAree@users.noreply.github.com>
2024-09-24 20:37:32 +0800

    第三次修改
    Signed-off-by: Aree <93140966+ScirAree@users.noreply.github.com>

--- LinkedListAlgorithm/linkedListCode/part03/Solution03.java
+++ LinkedListAlgorithm/linkedListCode/part03/Solution03.java
@@ -1,8 +1,6 @@
 import linkedListCode.ListNode;
 
-import java.util.List;
-
-import static linkedListCode.part01.Solution.*;
+import static linkedListCode.part01.Solution.createSingleLinkedList;
 
 //链表的花式反转方法
 
@@ -36,7 +34,45 @@ public static ListNode reverseListRecursion(ListNode head){
     return last;
 }
 
+
+//反转链表前N个节点 递归实现
+static ListNode successor = null;
+
+//反转链表前N个节点 迭代实现
+public static ListNode reverseN(ListNode head, int n) {
+
+    if (head == null || head.next == null) {
+        return head;
+    }
+    ListNode pre, cur, nxt;
+    cur = head;
+    pre = null;
+    nxt = head.next;
+    while (n > 0) {
+        cur.next = pre;
+        pre = cur;
+        cur = nxt;
+        nxt = nxt.next;
+        n--;
+    }
+    head.next = cur;
+    return pre;
+}
+
+public static ListNode reverseNRecursion(ListNode head, int n) {
+    if (n == 1) {
+        successor = head.next;
+        return head;
+    }
+    ListNode last = reverseNRecursion(head.next, n - 1);
+    head.next.next = head;
+    head.next = successor;
+    return last;
+}
+
+
 public static void main(String[] args) {
     ListNode l1 = createSingleLinkedList(new int[]{1,8,5,3,9,6,76,45,454,4,5,5,5,6});
     ListNode l2 = createSingleLinkedList(new int[]{2,3,4});
+    ListNode head = reverseNRecursion(l2, 4);
 }
